393588f679bc32016afc946fb3fd9a2d714e0d60
ChillerDragon <ChillerDragon@gmail.com>
2018-09-15 19:01:49 +0200

    Update git paths

--- bash_recreate_db.sh
+++ bash_recreate_db.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 cd /var/www/;
-rm EggsChange/EggsChange.db;
-cd /var/www/html/;
+rm EggsChange-scripts/EggsChange.db;
+cd /var/www/html/EggsChange-web;
 php recreate_all_tables.php;
-chown www-data:www-data /var/www/EggsChange/EggsChange.db;
-chown www-data:www-data /var/www/EggsChange/; #only needed once but it is NEEDED
-
+chown www-data:www-data /var/www/EggsChange-scripts/EggsChange.db;
+chown www-data:www-data /var/www/EggsChange-scripts/; #only needed once but it is NEEDED
